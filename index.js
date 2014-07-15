0205efc89cda6f5aea749d9739113ffd2519038c
Nicholas Kostelnik <nkostelnik@gmail.com>
2014-07-15 16:20:59 +0100

    using faye

--- index.js
+++ index.js
@@ -1,13 +1,16 @@
-var service = require('./service')
-var request = require('request')
+var request   = require('request'),
+    faye      = require('faye'),
+    settings  = require('env-settings')
+;
 
 function changeLightState(state) {
   var textState = state ? "On" : "Off"
   var message = "Switching Lights " + textState
-  console.log(message)
+  console.log(message);
 
-  service.talk(message)
-  
+  var client = new faye.Client(settings.brain + '/');
+  client.publish('/responses', message);
+
   request.post("http://localhost:5000/api/device/Lights?state=" + textState.toLowerCase(), function(err) {
     if (err) {
       return console.error("Couldnt post to light server", err)
@@ -16,26 +19,35 @@ function changeLightState(state) {
 }
 
 function toggleLight() {
+  console.log("Toggling Lights...");
   request.get("http://localhost:5000/api/device/Lights", function(err, response) {
     var lightState = JSON.parse(response.body).state
     changeLightState(!lightState)
   });
 }
 
-service.start("light_control", function(command) {
-  console.log(command)
+function start() {
+  var client = new faye.Client(settings.brain + '/');
+  client.subscribe('/light_control', function(message) {
+    var command = JSON.parse(message);
+    if (command.commands.on_off != undefined) {
+      for (var i = 0; i < command.commands.on_off.length; i++) {
+        var lightCommand = command.commands.on_off[i];
 
-  if (command.on_off != undefined) {
-    if (command.on_off.value == 'toggle') {
-      toggleLight()
-    }
+        if (lightCommand.value == 'toggle') {
+          toggleLight()
+        }
 
-    if (command.on_off.value == 'on') {
-      changeLightState(1)
-    }
+        if (lightCommand.value == 'on') {
+          changeLightState(1)
+        }
 
-    if (command.on_off.value == 'off') {
-      changeLightState(0)
+        if (lightCommand.value == 'off') {
+          changeLightState(0)
+        }
+      }
     }
-  }
-});
+  });
+}
+
+start();
